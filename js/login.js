964ffcb682ef0ebf6796365d446d912b741f316f
NaibDihan <afmnaibuddin@gmail.com>
2021-09-06 17:04:30 +0600

    Funtionality Added

---
+++ js/login.js
@@ -0,0 +1,11 @@
+document.getElementById('login-submit').addEventListener('click',function(){
+     if(document.getElementById('email-input').value=="abc@gmail.com" && document.getElementById('pass-input').value=="lolzi")
+     {
+         window.location.href='deposit.html';
+     }
+     else{
+         alert('Enter Valid Email and Password');
+     }
+
+});
+
